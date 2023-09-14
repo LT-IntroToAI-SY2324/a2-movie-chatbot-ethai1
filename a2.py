e1e72151924311ce46da816e2c6937edd0f216f5
Eric <ethai1@cps.edu>
2023-09-14 11:31:29 -0500

    worked on match

--- a2.py
+++ a2.py
@@ -14,40 +14,60 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
         None if the pattern and source do not "match" ELSE A list of matched words
         (words in the source corresponding to _'s or %'s, in the pattern, if any)
     """
-    sind = 0  # current index we are looking at in source list
-    pind = 0  # current index we are looking at in pattern list
+    source_index = 0  # current index we are looking at in source list
+    pattern_index = 0  # current index we are looking at in pattern list
     result: List[str] = []  # to store substitutions we will return if matched
 
     # keep checking as long as we haven't hit the end of either pattern or source while
     # pind is still a valid index OR sind is still a valid index (valid index means that
     # the index is != to the length of the list)
-    while "FILL IN CONDITION HERE":
-        # your job is to fill out the body of this loop
-
-        # you should delete the following line
-        return ["Not done yet :)"]
-
+    while source_index < len(source) and pattern_index < len(pattern):
         # 1) if we reached the end of the pattern but not source
-
+        if pattern_index == len(pattern) - 1 and source_index < len(source) - 1:
+            print("PATTERN IS GREATER THAN SOURCE. RETURNING NULL")
+            return None
+        
         # 2) if the current thing in the pattern is a %
         # WARNING: this condition contains the bulk of the code for the assignment
         # If you get stuck on this one, we encourage you to attempt the other conditions
         #   and come back to this one afterwards
 
-        # 3) if we reached the end of the source but not the pattern
+        elif pattern[pattern_index] == "%":
+            end_of_pattern = pattern[pattern_index + 1]
+            pattern_index += 1
+            
+            while source[source_index] != end_of_pattern:
+                result.append(source[source_index])
+                source_index += 1
+            
 
+        # 3) if we reached the end of the source but not the pattern
+        elif source_index == len(source) - 1 and pattern_index < len(pattern) - 1:
+            print("SOURCE IS LARGER THAN PATTERN. RETURNING NULL")
+            return None
+        
         # 4) if the current thing in the pattern is an _
+        elif pattern[pattern_index] == "_":
+            result.append(source[source_index])
 
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source
+        elif source[source_index] == pattern[pattern_index]:
+            source_index += 1
+            pattern_index += 1
 
         # 6) else : this will happen if none of the other conditions are met it
         # indicates the current thing it pattern doesn't match the current thing in
         # source
+        else:
+            print("ELSE RETURNING NULL")
+            return None
 
+    print(result)
     return result
 
 
+# TEST 5, INFINITE LOOP
 if __name__ == "__main__":
     assert match(["x", "y", "z"], ["x", "y", "z"]) == [], "test 1 failed"
     assert match(["x", "z", "z"], ["x", "y", "z"]) == None, "test 2 failed"
